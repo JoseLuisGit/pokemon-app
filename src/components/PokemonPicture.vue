ed13bb76830787cea77cd8d50172298f8ae32a6f
JoseLuisGit <joseluisstacruz@gmail.com>
2022-01-23 10:44:35 -0400

    design components

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,13 +1,38 @@
 <template>
-  <p>Esta es una imagen</p>
+  <div class="pokemon-container">
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      alt="Pokemon"
+    />
+
+    <img
+      class="hidden-pokemon"
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      alt="Pokemon"
+    />
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style scoped>
+.pokemon-container {
+  height: 200px;
+}
+img {
+  height: 200px;
+  position: absolute;
+  width: 100vw;
+  margin-left: -50vw;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+.hidden-pokemon {
+  filter: brightness(0);
+}
 </style>
